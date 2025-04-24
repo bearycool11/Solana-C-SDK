fab8bc4aa0730e7a77dd2c7f76c435447ed21579
Dr Josef Kurk Edwards <joed6834@colorado.edu>
2025-04-24 03:55:54 -0400

    Create Ledgerinterface.cpp
    Brb on this

---
+++ Solc/Src/Ledgerinterface.cpp
@@ -0,0 +1,9 @@
+#include "solc/LedgerInterface.hpp"
+
+namespace solc {
+
+// If you have a concrete implementation (e.g. using solana_sdk.h),
+// implement writeAccount, solLog, sendBugReward here.
+// For a BPF program, you'd call sol_log and system_instruction::transfer.
+
+} // namespace solc
